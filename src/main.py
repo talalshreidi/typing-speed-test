8da339745dc221c3d214400e97cef0acce9e3d69
talalshreidi <talalshreidi77@gmail.com>
2025-10-01 10:37:41 -0400

    initialize main application

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Typing Speed Test - Talal Shreidi")
+    root.geometry("800x400")
+    root.resizable(False, False)
+    root.configure(bg="#f0f0f0")
+    root.mainloop()
