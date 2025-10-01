955c069ba385cb0c645196cd69f9d0ce9cba0004
talalshreidi <talalshreidi77@gmail.com>
2025-10-01 10:55:40 -0400

    refactor: simplify TypingTestUI initialization by inheriting from tk.Tk

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-import tkinter as tk
 from ui import TypingTestUI
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = TypingTestUI(root)
-    root.mainloop()
+    app = TypingTestUI()
+    app.mainloop()
--- src/ui.py
+++ src/ui.py
@@ -1,15 +1,18 @@
 
+import tkinter as tk
 
-
-class TypingTestUI:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Typing Speed Test - Talal Shreidi")
-        self.master.geometry("800x400")
-        self.master.resizable(False, False)
-        self.master.configure(bg="#f0f0f0")
+class TypingTestUI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Typing Speed Test - Talal Shreidi")
+        self.geometry("800x400")
+        self.resizable(False, False)
+        self.configure(bg="#f0f0f0")
         self.setup_ui()
 
 
     def setup_ui(self):
+        
+        # Passage Display
+
         pass
